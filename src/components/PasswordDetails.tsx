ed96dc111bde1834af43596926bbdf02835cae01
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 17:37:58 +0300

    create the PasswordDetails component

---
+++ src/components/PasswordDetails.tsx
@@ -0,0 +1,41 @@
+import FormWrapper from "./FormWrapper";
+
+interface Password {
+  password: string;
+  confirmPassword: string;
+}
+
+interface PasswordDetailsprops extends Password {
+  updateFields: (fields: Partial<Password>) => void;
+}
+
+function PasswordDetails({
+  password,
+  confirmPassword,
+  updateFields,
+}: PasswordDetailsprops) {
+  return (
+    <FormWrapper title="User Password">
+      <label htmlFor="password">Password :</label>
+      <input
+        type="password"
+        id="password"
+        autoFocus
+        value={password}
+        onChange={(e) => updateFields({ password: e.target.value })}
+        required
+      />
+
+      <label htmlFor="confirm-password">Password :</label>
+      <input
+        type="password"
+        id="confirm-password"
+        value={confirmPassword}
+        onChange={(e) => updateFields({ confirmPassword: e.target.value })}
+        required
+      />
+    </FormWrapper>
+  );
+}
+
+export default PasswordDetails;
