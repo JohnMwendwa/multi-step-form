2f1462d782f23c14cbc9bfd7a41daa4504a9bb15
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 21:30:31 +0300

    style and display the user details for confirmation before submiting

--- src/components/ConfirmDetailsPage.tsx
+++ src/components/ConfirmDetailsPage.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import styled from "styled-components";
 
-function ConfirmDetailsPage() {
-  return <div>ConfirmDetailsPage</div>;
+const Title = styled.h2`
+  text-align: center;
+  margin: 0;
+  margin-bottom: 2rem;
+`;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 0.2rem 0.5rem;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: auto minmax(auto, 400px);
+
+  h3 {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+interface ConfirmDetailsPageProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+function ConfirmDetailsPage({
+  firstName,
+  lastName,
+  email,
+  password,
+}: ConfirmDetailsPageProps) {
+  return (
+    <>
+      <Title>Confirm Details</Title>
+      <Wrapper>
+        <h3>First Name :</h3>
+        <p>{firstName}</p>
+        <h3>Last Name :</h3>
+        <p>{lastName}</p>
+        <h3>Email :</h3>
+        <p>{email}</p>
+        <h3>Password</h3>
+        <p>{password}</p>
+      </Wrapper>
+    </>
+  );
 }
 
 export default ConfirmDetailsPage;
