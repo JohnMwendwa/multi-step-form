2384e0d8b92fc890fb636e47262e521bda800fb6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 15:03:16 +0300

    create a container to house all other elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 function App() {
-  return <div></div>;
+  return <Container></Container>;
 }
 
 export default App;
