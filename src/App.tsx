495cc0bda341178d2f5b4af88faa87bab1a303a3
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 15:56:54 +0300

    create an interface of the expected user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+interface UserData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const INITIAL_DATA: UserData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Container = styled.div``;
 
 function App() {
+  const [data, setData] = useState(INITIAL_DATA);
+
   return <Container></Container>;
 }
 
