4da9d310656e41cd7ad62d43c81e543ffcbc0301
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 15:38:34 +0300

    style the title and children props

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -5,14 +5,23 @@ interface FormWrapperProps {
   title: string;
   children: ReactNode;
 }
-
-const Wrapper = styled.div``;
+const Title = styled.h2`
+  text-align: center;
+  margin: 0;
+  margin-bottom: 2rem;
+`;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 1rem 0.5rem;
+  justify-content: flex-start;
+  grid-template-columns: auto minmax(auto, 400px);
+`;
 
 export default function FormWrapper({ title, children }: FormWrapperProps) {
   return (
-    <Wrapper>
-      <h2>{title}</h2>
-      <div>{children}</div>
-    </Wrapper>
+    <>
+      <Title>{title}</Title>
+      <Wrapper>{children}</Wrapper>
+    </>
   );
 }
