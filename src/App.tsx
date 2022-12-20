3af2e70505348bdf81fc3a6ed5c254613f086055
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 11:58:53 +0300

    delete practice code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React, { useState } from "react";
-
-import "./App.css";
-import ShoppingList from "./components/ShoppingList";
-import Item from "./models/item";
-import ShoppingListForm from "./components/ShoppingListForm";
+import React from "react";
 
 function App() {
-  const [items, setItems] = useState<Item[]>([]);
-
-  const addItem = (item: string) => {
-    const idx = Math.floor(Math.random() * 1000);
-
-    setItems([...items, { product: item, id: idx, quantity: 1 }]);
-  };
-  return (
-    <div>
-      <ShoppingList items={items} />
-      <ShoppingListForm onAddItem={addItem} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
