eab8087e97d072b30b92a468b3885306ff787e2d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 11:56:23 +0300

    delete unnecessary initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import ShoppingList from "./components/ShoppingList";
+import Item from "./models/item";
+import ShoppingListForm from "./components/ShoppingListForm";
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const addItem = (item: string) => {
+    const idx = Math.floor(Math.random() * 1000);
+
+    setItems([...items, { product: item, id: idx, quantity: 1 }]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ShoppingList items={items} />
+      <ShoppingListForm onAddItem={addItem} />
     </div>
   );
 }
