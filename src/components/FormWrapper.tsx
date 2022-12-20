7899154343189dfa177604a2c0682584b18a3e37
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 22:12:50 +0300

    style the inputs

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -14,7 +14,13 @@ const Wrapper = styled.div`
   display: grid;
   gap: 1rem 0.5rem;
   justify-content: flex-start;
+  align-items: center;
   grid-template-columns: auto minmax(auto, 400px);
+
+  input {
+    padding: 3px 10px;
+    line-height: 1.25rem;
+  }
 `;
 
 export default function FormWrapper({ title, children }: FormWrapperProps) {
