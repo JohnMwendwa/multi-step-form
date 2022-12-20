658b90fa502b11ce0644fc49e0dea58886655c16
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-20 16:29:09 +0300

    create and style the AccountDetailsForm component

---
+++ src/components/AccountDetailsForm.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import FormWrapper from "./FormWrapper";
+
+interface UserData {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+interface AccountDetailsProps extends UserData {
+  updateFields: (fields: Partial<UserData>) => void;
+}
+
+function AccountDetailsForm({
+  firstName,
+  lastName,
+  updateFields,
+}: AccountDetailsProps) {
+  return (
+    <FormWrapper title="User Details">
+      <label htmlFor="first-name">First Name :</label>
+      <input
+        type="text"
+        required
+        autoFocus
+        value={firstName}
+        onChange={(e) => updateFields({ firstName: e.target.value })}
+        id="first-name"
+      />
+
+      <label htmlFor="last-name">Last Name :</label>
+      <input
+        type="text"
+        id="last-name"
+        value={lastName}
+        onChange={(e) => updateFields({ lastName: e.target.value })}
+        required
+      />
+    </FormWrapper>
+  );
+}
+
+export default AccountDetailsForm;
